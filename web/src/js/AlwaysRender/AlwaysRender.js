5cdbadf6bef63cf886418dfc966974cd29dab335
System Administrator <gokarna.adhikari14@gmail.com>
2022-08-09 19:08:37 +0545

    Created the skeletion for pages

---
+++ web/src/js/AlwaysRender/AlwaysRender.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AlwaysRender =()=>{
+    return(
+        <>
+            <h5>This portion will always render...</h5>
+
+            <i>Put the modules below this !</i>
+
+            {/* eg <Home></Home> */}
+
+        </>
+    )
+}
+
+export default AlwaysRender;
