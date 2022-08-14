4ed5df0aea8b2a0ea65e2fbb34d2b50cb715b912
kshitiz <kshitiz.dhakal50@gmail.com>
2022-08-14 23:16:26 +0545

    Title Font for homepage added

--- web/src/js/home/Home.js
+++ web/src/js/home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import '../home/homepage.css'
 
 const Home =()=>{
 
     return(
         <>
-            This is silence and kshitiz home.
+            <h1 className="title">Speech Text Summerization and Data Mining using NLP</h1>
             <marquee> This is our homepage</marquee>
         </>
     )
