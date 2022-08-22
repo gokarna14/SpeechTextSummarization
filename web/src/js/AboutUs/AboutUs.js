9b673bec9c6da66116819e6a46012daeb5a15f34
Abhay <abhay.nepal.9@gmail.com>
2022-08-22 20:42:41 +0545

    added photos in about us

--- web/src/js/AboutUs/AboutUs.js
+++ web/src/js/AboutUs/AboutUs.js
@@ -1,11 +1,20 @@
 import React from "react";
-
+import abhay from "../AboutUs/files/abhay.jpg"
+import style from './files/custom.css'
+import silence from "../AboutUs/files/silence.jpg"
+import gokarna from "../AboutUs/files/gokarna.jpg"
+import kshitiz from "../AboutUs/files/kshitiz.jpg"
 const AboutUs =()=>{
 
     return(
         <>
-            This is AboutUs  and our project.
-            <marquee> This is abhay in About us page</marquee>
+           <div>
+            <img id ='img1' src= {abhay} style={{style}}/>
+            <img id = 'img1'src= {silence} style={{style}}/>
+            <img id='ktaru' src= {kshitiz} style={{style}} />
+            <img id='ktaru' src= {gokarna} style={{style}} />
+           </div>
+
         </>
     )
 
