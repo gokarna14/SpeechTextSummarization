090509d6238833ebddc126af893aa9ad252022cb
Abhay <abhay.nepal.9@gmail.com>
2022-08-23 13:53:52 +0545

    same

--- web/src/js/AboutUs/AboutUs.js
+++ web/src/js/AboutUs/AboutUs.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import abhay from "../AboutUs/files/abhay.jpg"
-// import style from './files/custom.css'
-// import silence from "../AboutUs/files/silence.jpg"
-// import gokarna from "../AboutUs/files/gokarna.jpg"
-// import kshitiz from "../AboutUs/files/kshitiz.jpg"
+import abhay from "../AboutUs/files/abhay.jpg"
+import style from './files/custom.css'
+import silence from "../AboutUs/files/silence.jpg"
+import gokarna from "../AboutUs/files/gokarna.jpg"
+import kshitiz from "../AboutUs/files/kshitiz.jpg"
 const AboutUs =()=>{
 
     return(
