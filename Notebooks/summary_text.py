55dc541b823b971b6212f10437ac62fc2fe76b84
System Administrator <gokarna.adhikari14@gmail.com>
2022-09-16 20:14:40 +0545

    Made a minor change

--- Notebooks/summary_text.py
+++ Notebooks/summary_text.py
@@ -28,13 +28,6 @@ lsa = svd.fit_transform(bag_of_words)
 # Using pandas to look at the output of lsa
 
 
-
-
-
-
-
-
-
 topic_encoded_df = pd.DataFrame(lsa, columns=["topic1", "topic2"])
 topic_encoded_df["doc"] = doc
 
